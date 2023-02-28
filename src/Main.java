36c8e9d4eb3d21cfa5c768ca9c11bf418d74b067
User <nazarovtolya2@gmail.com>
2023-02-28 21:32:54 +0500

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {;
         task1();
         task2();
+        task3();
 
     }
     public static int[] generateRandomArray() {
@@ -15,16 +16,15 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        int [] arr = generateRandomArray();
+        int[] arr = generateRandomArray();
         int sum = 0;
-        for (int index = 0; index < arr.length; index++) {
-            sum = sum + arr.length;
+        for (int element : arr) {
+            sum += element;
         }
-        System.out.println("Сумма выплат за месяц составит " + sum);
-
+            System.out.println("Сумма выплат за месяц составит " + sum);
     }
 
-    public static void task2() {
+        public static void task2() {
         System.out.println("Задача 2");
         int [] arr = generateRandomArray();
         int maxCash = 0;
@@ -42,4 +42,16 @@ public class Main {
         }
         System.out.println("Минимальная выплата составит " + minCash);
     }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] arr = generateRandomArray();
+        double sum = 0;
+        for (double element : arr) {
+            sum += element;
+        }
+        double mediumCash = sum / arr.length;
+        System.out.println("Средняя выплата в месяц составляет " + mediumCash);
+    }
+
 }

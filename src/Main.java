2e9fae83d73c008fb886cd564b37ffb14f91fe02
User <nazarovtolya2@gmail.com>
2023-02-28 20:55:15 +0500

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {;
+        task1();
+
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int [] arr = generateRandomArray();
+        int sum = 0;
+        for (int index = 0; index < arr.length; index++) {
+            sum = sum + arr.length;
+        }
+        System.out.println("Сумма выплат за месяц составит " + sum);
+
+    }
+}
